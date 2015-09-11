ec3a6570191320dbea77f4e8bc5750c2df9c37be
[angela riggs] <riggs.ang@gmail.com>
2015-09-10 21:01:44 -0700

    created array of gem objects, used ng-repeat to display them on the site

--- angular-notes.md
+++ angular-notes.md
@@ -1,8 +1,16 @@
 #Angular Notes from Codeschool Course
 
-##Directive:
+##Directives:
 - Marker on an HTML tag that tells Angular to run/reference JS code
-- How we bind behavior
+- HTML annotations that trigger JS behaviors
+- How we bind JS behavior to HTML
 
 ##Expressions:
 - Allow you to insert dynamic values into your HTML
+- How values get displayed within the page
+
+##Controllers:
+- Where we define our app's behavior by defining functions and values
+
+##Modules:
+- Where the application components live
--- app.js
+++ app.js
@@ -1,2 +1,33 @@
-var app = angular.module('store',[]);
-// 'store' = application name; [] are where dependencies will go.
+(function(){
+	// 'store' = application name; [] are where dependencies will go.
+	var app = angular.module('gemStore',[]);
+	
+	//name of controller is Cap Case and always uses name Controller
+	app.controller('StoreController', function(){
+		this.products = gems;
+	});
+
+	var gems = [
+		{
+			name:'Azurite',
+			price: 2.95,
+			description: 'Aborably wrinkled pit pup faces',
+			canPurchase: true,
+			soldOut: false
+		},
+		{
+			name:'Emerald',
+			price: 6.88,
+			description: 'Green as the glacier waters of Canada',
+			canPurchase: true,
+			soldOut: false
+		},
+		{
+			name:'Azurite',
+			price: 8.99,
+			description: 'The color of love in Oregon',
+			canPurchase: true,
+			soldOut: false
+		}
+	];
+})();
