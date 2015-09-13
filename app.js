11f599947cedeeade08c9a076807262f87bbf0d9
[angela riggs] <riggs.ang@gmail.com>
2015-09-13 13:01:00 -0700

    added array of reviews to product, giving me product.reviews. created form with drop-down for stars, freehand text, and email, along w/submit button in html

--- app.js
+++ app.js
@@ -13,7 +13,7 @@
 	      }
 	    };
 	  });
-	
+
 	//name of controller is Cap Case and always uses name Controller
 	app.controller('StoreController', function(){
 		this.products = gems;
@@ -25,21 +25,75 @@
 			price: 2,
 			description: 'Aborably wrinkled pit pup faces',
 			canPurchase: true,
-			soldOut: false
+			soldOut: false,
+			images: [],
+			reviews: [
+				{
+					stars: 5,
+					body: "I love the Azurite stone!",
+					author: "jack@jill.com"
+				},
+				{
+					stars: 4,
+					body: "Azurite rocks!",
+					author: "tom@thumb.com"
+				},
+				{
+					stars: 1,
+					body: "Azurite sucks!",
+					author: "peter@pumpkineater.com"
+				}
+			]
 		},
 		{
 			name:'Emerald',
 			price: 6.88,
 			description: 'Green as the glacier waters of Canada',
 			canPurchase: true,
-			soldOut: false
+			soldOut: false,
+			images: [],
+			reviews: [
+				{
+					stars: 5,
+					body: "Emerald is great!",
+					author: "river@song.com"
+				},
+				{
+					stars: 3,
+					body: "Emerald is just okay.",
+					author: "rose@blondie.com"
+				},
+				{
+					stars: 1,
+					body: "Emerald sucks!",
+					author: "martha@badass.com"
+				}
+			]
 		},
 		{
-			name:'Azurite',
+			name:'Sunstone',
 			price: 8.99,
 			description: 'The color of love in Oregon',
 			canPurchase: true,
-			soldOut: false
+			soldOut: false,
+			images: [],
+			reviews: [
+				{
+					stars: 5,
+					body: "Sunstone is great!",
+					author: "robert@heinlein.com"
+				},
+				{
+					stars: 3,
+					body: "Sunstone is just okay.",
+					author: "robert@parker.com"
+				},
+				{
+					stars: 1,
+					body: "Sunstone sucks!",
+					author: "spider@robinson.com"
+				}
+			]
 		}
 	];
 
